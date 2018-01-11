6fd53d0b068cb1dcd0b5a06b3f21c7724c08f139
davidajoyce <davidajoyce141@gmail.com>
2018-01-11 13:46:40 +0000

    testing a bbc news api from newsapi and create an array of tuples that hold info from top headlines

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import rest_newsapi.py as rnews
+import rest_newsapi as rnews
+
+
+def main():
+	
+	url = ('https://newsapi.org/v2/top-headlines?'
+       'sources=bbc-news&'
+       'apiKey=97c9e17e34af48ba9768758c481e45f3')
+
+	rnews.create_url_info(url)
+
+
+
+
+if __name__=='__main__':
+
+	main()
--- rest_newsapi.py
+++ rest_newsapi.py
@@ -2,8 +2,6 @@ import urllib2
 import sys
 import requests
 
-
-
 def is_URL_valid(URL):
 
 	try:
@@ -15,6 +13,31 @@ def is_URL_valid(URL):
 	except urllib2.URLError, ex:
 		sys.stderr.write("This URL is not valid")
 		return False
+
+
+def create_tuple(article):
+	tup = (article['source']['id'],article['title'],article['url'],article['description'],article['publishedAt'])
+
+	return tup
+
+def create_url_info(url):
+
+	response = requests.get(url)
+	json_dict = response.json()
+	list_art = []
+
+	for i in range(len(json_dict['articles'])):
+		
+		info_tuple = create_tuple(json_dict['articles'][i])
+		list_art.append(info_tuple)
+
+	
+
+		
+
+
+
+
 
 
 
